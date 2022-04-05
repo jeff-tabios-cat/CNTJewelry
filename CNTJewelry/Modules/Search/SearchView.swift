978fdf8fc53582123d5da1456014a2cc3a2cf44f
Jeff <>
2022-04-05 18:08:50 +0800

    search view style and view model

---
+++ CNTJewelry/Modules/Search/SearchView.swift
@@ -0,0 +1,47 @@
+//
+//  SearchView.swift
+//  CNTJewelry
+//
+//  Created by Jeffrey Tabios on 4/5/22.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    
+    @ObservedObject var viewModel = SearchViewModel()
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                HStack {
+                    TextField("Search CNT", text: $viewModel.query)
+                    Image("icon-black-search")
+                        .tint(.white)
+                        .foregroundColor(.white)
+                }
+                .frame(width: 300)
+                List(viewModel.results) { result in
+                    SearchResultRow(searchResult: result)
+                }
+                Spacer()
+            }
+        }
+        
+    }
+}
+
+// A view that shows the data for one Restaurant.
+struct SearchResultRow: View {
+    var searchResult: SearchResult
+
+    var body: some View {
+        Text("\(searchResult.text)")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
---
+++ CNTJewelry/Modules/Search/SearchViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewModel.swift
+//  CNTJewelry
+//
+//  Created by Jeffrey Tabios on 4/5/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct SearchResult: Identifiable {
+    let id = UUID()
+    let text: String
+}
+
+final class SearchViewModel: ObservableObject {
+    @Published var query: String = "This is a search string"
+    @Published var results: [SearchResult] = [SearchResult(text: "result 1"),
+                                              SearchResult(text: "result 2"),
+                                              SearchResult(text: "result 3")]
+}
